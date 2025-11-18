3306d4adbd72b694987f0d3e51d84861f69ff726
Apothecary1995 <ahmet.cengiz19951995@gmail.com>
2025-11-18 12:32:20 +0100

    broken still

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,25 +1,25 @@
 // config/db.config.js
 require('dotenv').config();
 
-// Bu dosyayı, Sequelize'ın doğrudan DATABASE_URL'yi kullanmasını sağlayacak 
-// şekilde basitleştiriyoruz.
-// Localde çalışmak için bu ayarları kullanmaya devam edebiliriz.
+
+const isProduction = process.env.NODE_ENV === 'production'; 
 
 module.exports = {
-  // Render bu değişkeni otomatik olarak 'production' olarak tanımlar.
-  isProduction: process.env.NODE_ENV === 'production',
-
-  // Local/Geliştirme Ortamı Ayarları
   HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
+ 
+  dialect: isProduction ? "postgres" : "mysql",
   
-  // Eğer production (Render) ise, varsayılan olarak PostgreSQL kullanırız.
-  // Localde (development) ise .env dosyanızdaki ayar (büyük ihtimalle mysql) kullanılır.
-  dialect: process.env.DB_DIALECT || 'postgres', // DB_DIALECT .env'de tanımlı değilse postgres kullanır
+  
+  dialectOptions: isProduction ? {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
+    }
+  } : {}, 
 
-  // Diğer ayarlar...
   pool: {
     max: 5,
     min: 0,
