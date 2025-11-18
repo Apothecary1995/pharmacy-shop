30e773502e846a1765f0fc72c149a0b917aedd15
Apothecary1995 <ahmet.cengiz19951995@gmail.com>
2025-11-18 12:48:22 +0100

    trying

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,41 +1,28 @@
+// models/index.js (Ana Bağlantı)
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
+let sequelize;
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  pool: { ...dbConfig.pool }
-});
-
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-
-// Modelleri yükle
-db.users = require("./user.model.js")(sequelize, Sequelize);
-db.drugs = require("./drug.model.js")(sequelize, Sequelize);
-db.prescriptions = require("./prescription.model.js")(sequelize, Sequelize);
-db.orders = require("./order.model.js")(sequelize, Sequelize);
-db.orderItems = require("./orderItem.model.js")(sequelize, Sequelize);
-
-
-db.users.hasMany(db.prescriptions, { foreignKey: "userId" });
-db.prescriptions.belongsTo(db.users, { foreignKey: "userId" });
-
-db.users.hasMany(db.orders, { foreignKey: "userId" });
-db.orders.belongsTo(db.users, { foreignKey: "userId" });
-
-
-db.prescriptions.hasOne(db.orders, { foreignKey: "prescriptionId" });
-db.orders.belongsTo(db.prescriptions, { foreignKey: "prescriptionId" });
-
-
-db.orders.hasMany(db.orderItems, { foreignKey: "orderId" });
-db.orderItems.belongsTo(db.orders, { foreignKey: "orderId" });
-
-
-db.drugs.hasMany(db.orderItems, { foreignKey: "drugId" });
-db.orderItems.belongsTo(db.drugs, { foreignKey: "drugId" });
-
-
-module.exports = db;
+// Render'da DATABASE_URL var mı diye kontrol et
+if (process.env.DATABASE_URL) {
+    // Canlı: DATABASE_URL, PostgreSQL ve SSL kullan
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        pool: { ...dbConfig.pool }
+    });
+} else {
+    // Yerel: Eski dbConfig yapısını kullan (MySQL)
+    sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        pool: { ...dbConfig.pool }
+    });
+}
+// ... (Modeller ve ilişkiler bu kısmın altında devam eder)
+// ...
