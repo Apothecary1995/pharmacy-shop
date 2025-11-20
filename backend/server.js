592cf43daafc3cd7292c9964d155bc77065cfed1
Apothecary1995 <ahmet.cengiz19951995@gmail.com>
2025-11-20 01:37:00 +0100

    fix

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/images', express.static(path.join(__dirname, 'images'))); // SQL'de /images/ kullandık
 
 const db = require("./models");
-db.sequelize.sync()
+db.sequelize.sync({force:true})
   .then(() => {
     console.log("Database synced successfully.");
   })
