b9a6daa5cadcf96c8d69ba5921412154a75fcfae
Apothecary1995 <104367082+Apothecary1995@users.noreply.github.com>
2025-11-29 10:07:38 +0100

    Update payment.routes.js

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const router = express.Router();
 const controller = require("../controllers/payment.controller");
 const { authJwt } = require("../middleware");
 
-
-router.use((req, res, next) => {
-    res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-});
-
-router.post('/intent', [authJwt.verifyToken], controller.createPaymentIntent);
-
-module.exports = router;
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+    app.post("/api/payments/intent", [authJwt.verifyToken], controller.createPaymentIntent); 
+};
